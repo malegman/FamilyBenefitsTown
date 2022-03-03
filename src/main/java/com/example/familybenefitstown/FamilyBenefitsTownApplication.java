b3b1c5dffcb81fe5376a5c3b7126a3d3bce97939
malegman <smegovic@gmail.com>
2022-03-03 20:25:27 +0500

    Initial commit

---
+++ src/main/java/com/example/familybenefitstown/FamilyBenefitsTownApplication.java
@@ -0,0 +1,13 @@
+package com.example.familybenefitstown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyBenefitsTownApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FamilyBenefitsTownApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/familybenefitstown/FamilyBenefitsTownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.familybenefitstown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyBenefitsTownApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
