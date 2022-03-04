8f8ec38d08bd358b4870bc12c8ceb26ebb0d8be9
malegman <smegovic@gmail.com>
2022-03-04 12:21:03 +0500

    Добавление конфигурации логирования

--- src/main/java/com/example/familybenefitstown/FamilyBenefitsTownApplication.java
+++ src/main/java/com/example/familybenefitstown/FamilyBenefitsTownApplication.java
@@ -1,5 +1,6 @@
 package com.example.familybenefitstown;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FamilyBenefitsTownApplication {
 
   public static void main(String[] args) {
+    PropertyConfigurator.configure("classpath:log4j.properties");
     SpringApplication.run(FamilyBenefitsTownApplication.class, args);
   }
 
