527d991e4c69125996cb5d2762cee1f20b0e4f29
malegman <smegovic@gmail.com>
2022-03-05 23:49:41 +0500

    Переименование переменной адреса и удаление метода на получение пустых данных

--- src/main/java/com/example/familybenefitstown/security/web/auth/JwtUserData.java
+++ src/main/java/com/example/familybenefitstown/security/web/auth/JwtUserData.java
@@ -3,7 +3,6 @@ package com.example.familybenefitstown.security.web.auth;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -15,7 +14,7 @@ import java.util.regex.Pattern;
  */
 @Getter
 @Builder
-public class JwtAuthenticationUserData {
+public class JwtUserData {
 
   /**
    * ID пользователя
@@ -30,28 +29,14 @@ public class JwtAuthenticationUserData {
   /**
    * IP-адрес, с которого пользователь вошел в систему
    */
-  private String ipAddress;
-
-  /**
-   * Возвращает объект с пустыми данными пользователя, с null значениями и пустой коллекцией
-   * @return объект с пустыми данными пользователя
-   */
-  public static JwtAuthenticationUserData emptyUserData() {
-
-    return JwtAuthenticationUserData
-        .builder()
-        .idUser(null)
-        .nameRoleSet(Collections.emptySet())
-        .ipAddress(null)
-        .build();
-  }
+  private String address;
 
   /**
    * Преобразует строку в объект данных авторизации в jwt
    * @param content строка для преобразования
    * @return данные авторизации
    */
-  public static JwtAuthenticationUserData fromString(String content) {
+  public static JwtUserData fromString(String content) {
 
     Pattern patternAuth = Pattern.compile("^" +
                                               "id=(?<id>[0-9a-zA-Z]{20})" +
@@ -60,17 +45,17 @@ public class JwtAuthenticationUserData {
                                               "$");
     Matcher matcherAuth = patternAuth.matcher(content);
 
-    return JwtAuthenticationUserData
+    return JwtUserData
         .builder()
         .idUser(matcherAuth.group("id"))
         .nameRoleSet(new HashSet<>(List.of(matcherAuth.group("roles").split(","))))
-        .ipAddress(matcherAuth.group("ip"))
+        .address(matcherAuth.group("ip"))
         .build();
   }
 
   @Override
   public String toString() {
 
-    return String.format("id=%sroles=%sip=%s", idUser, String.join(",", nameRoleSet), ipAddress);
+    return String.format("id=%sroles=%sip=%s", idUser, String.join(",", nameRoleSet), address);
   }
 }
