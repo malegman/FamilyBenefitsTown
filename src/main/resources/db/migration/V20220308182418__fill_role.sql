1988ec51b55d3cb17c9c5899b17f193dc6cb07e5
malegman <smegovic@gmail.com>
2022-03-10 22:45:39 +0500

    Создание ролей с фиксированным id

--- src/main/resources/db/migration/V20220308182418__fill_role.sql
+++ src/main/resources/db/migration/V20220308182418__fill_role.sql
@@ -1,5 +1,5 @@
 INSERT INTO family_benefit_town.role
-  (name) VALUES
-        ('ROLE_USER'),
-        ('ROLE_ADMIN'),
-        ('ROLE_SUPER_ADMIN');
+  (id, name) VALUES
+        ('ID_USER', 'ROLE_USER'),
+        ('ID_ADMIN', 'ROLE_ADMIN'),
+        ('ID_SUPER_ADMIN', 'ROLE_SUPER_ADMIN');
