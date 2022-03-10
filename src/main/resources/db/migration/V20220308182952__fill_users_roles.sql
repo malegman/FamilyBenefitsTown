a7935e716dc6f0bef2cbdc53647bb488f6efef82
malegman <smegovic@gmail.com>
2022-03-10 22:45:18 +0500

    Изменение проверки ролей по id, не по названию

--- src/main/resources/db/migration/V20220308182952__fill_users_roles.sql
+++ src/main/resources/db/migration/V20220308182952__fill_users_roles.sql
@@ -2,5 +2,5 @@ INSERT INTO family_benefit_town.users_roles
 SELECT family_benefit_town.user.id, family_benefit_town.role.id
 FROM family_benefit_town.user, family_benefit_town.role
 WHERE family_benefit_town.user.email = 'smegovic@gmail.com' AND
-      (family_benefit_town.role.name = 'ROLE_SUPER_ADMIN' OR
-       family_benefit_town.role.name = 'ROLE_ADMIN');
+      (family_benefit_town.role.id = 'ID_SUPER_ADMIN' OR
+       family_benefit_town.role.id = 'ID_ADMIN');
