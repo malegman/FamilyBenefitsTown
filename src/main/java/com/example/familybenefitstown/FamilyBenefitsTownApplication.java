6cd11584ca477a4355532d2aa147b5d3d03e18a6
malegman <smegovic@gmail.com>
2022-03-12 17:10:29 +0500

    Удаление конфигурирования логирования из кода

--- src/main/java/com/example/familybenefitstown/FamilyBenefitsTownApplication.java
+++ src/main/java/com/example/familybenefitstown/FamilyBenefitsTownApplication.java
@@ -1,7 +1,5 @@
 package com.example.familybenefitstown;
 
-import com.example.familybenefitstown.resources.R;
-import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FamilyBenefitsTownApplication {
 
   public static void main(String[] args) {
-    PropertyConfigurator.configure(R.LOG_CONFIG_FILE_NAME);
     SpringApplication.run(FamilyBenefitsTownApplication.class, args);
   }
 
