ac408d3019c978e0f7f15c0a75fc49b02df189c3
malegman <smegovic@gmail.com>
2022-03-12 17:12:04 +0500

    Переименование пакета

--- src/main/java/com/example/familybenefitstown/dto/entities/RefreshTokenEntity.java
+++ src/main/java/com/example/familybenefitstown/dto/entities/RefreshTokenEntity.java
@@ -1,6 +1,5 @@
-package com.example.familybenefitstown.dto.entities.strong;
+package com.example.familybenefitstown.dto.entities;
 
-import com.example.familybenefitstown.dto.entities.ObjectEntity;
 import lombok.*;
 import org.springframework.lang.NonNull;
 
@@ -11,17 +10,17 @@ import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 /**
- * Модель записи таблицы "login_code"
+ * Модель записи таблицы "refresh_token"
  */
 @Entity
-@Table(name = "login_code", schema = "family_benefit_town")
+@Table(name = "refresh_token", schema = "family_benefit_town")
 @Getter
 @Setter
 @ToString
 @Builder
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 @AllArgsConstructor
-public class LoginCodeEntity extends ObjectEntity {
+public class RefreshTokenEntity extends ObjectEntity {
 
   /**
    * ID пользователя
@@ -32,13 +31,14 @@ public class LoginCodeEntity extends ObjectEntity {
   private String idUser;
 
   /**
-   * Код для входа в систему
+   * Токен доступа
    */
-  @Column(name = "code")
-  private int code;
+  @NonNull
+  @Column(name = "token")
+  private String token;
 
   /**
-   * Время истечения срока кода
+   * Время истечения срока токена восстановления
    */
   @NonNull
   @Column(name = "date_expiration")
