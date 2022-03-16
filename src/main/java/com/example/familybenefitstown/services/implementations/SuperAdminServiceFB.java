0ae3763981ac13f2e3c16ec07ef9328177fef411
malegman <smegovic@gmail.com>
2022-03-16 13:37:46 +0500

    Изменение работы с ролями и проверки существования пользователя по ID

--- src/main/java/com/example/familybenefitstown/services/implementations/SuperAdminServiceFB.java
+++ src/main/java/com/example/familybenefitstown/services/implementations/SuperAdminServiceFB.java
@@ -2,7 +2,6 @@ package com.example.familybenefitstown.services.implementations;
 
 import com.example.familybenefitstown.api_models.admin.AdminSave;
 import com.example.familybenefitstown.converters.AdminDBConverter;
-import com.example.familybenefitstown.dto.entities.RoleEntity;
 import com.example.familybenefitstown.dto.entities.UserEntity;
 import com.example.familybenefitstown.dto.repositories.UserRepository;
 import com.example.familybenefitstown.exceptions.AlreadyExistsException;
@@ -12,12 +11,12 @@ import com.example.familybenefitstown.resources.R;
 import com.example.familybenefitstown.resources.RDB;
 import com.example.familybenefitstown.security.generator.RandomValue;
 import com.example.familybenefitstown.security.services.interfaces.DBIntegrityService;
-import com.example.familybenefitstown.security.services.interfaces.TokenCodeService;
 import com.example.familybenefitstown.services.interfaces.MailService;
 import com.example.familybenefitstown.services.interfaces.SuperAdminService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Реализация сервиса, управляющего объектом "супер-администратор"
@@ -32,10 +31,6 @@ public class SuperAdminServiceFB implements SuperAdminService {
   private final UserRepository userRepository;
 
   /**
-   * Интерфейс сервиса для работы с токенами доступа (в формате jwt) и восстановления и кодом для входа
-   */
-  private final TokenCodeService tokenCodeService;
-  /**
    * Интерфейс сервиса, отвечающего за целостность базы данных
    */
   private final DBIntegrityService dbIntegrityService;
@@ -47,17 +42,14 @@ public class SuperAdminServiceFB implements SuperAdminService {
   /**
    * Конструктор для инициализации интерфейсов репозиториев и сервисов
    * @param userRepository репозиторий, работающий с моделью таблицы "user"
-   * @param tokenCodeService интерфейс сервиса для работы с токенами доступа (в формате jwt) и восстановления и кодом для входа
    * @param dbIntegrityService интерфейс сервиса, отвечающего за целостность базы данных
    * @param mailService интерфейс сервиса для отправки сообщений на электронную почту
    */
   @Autowired
   public SuperAdminServiceFB(UserRepository userRepository,
-                             TokenCodeService tokenCodeService,
                              DBIntegrityService dbIntegrityService,
                              MailService mailService) {
     this.userRepository = userRepository;
-    this.tokenCodeService = tokenCodeService;
     this.dbIntegrityService = dbIntegrityService;
     this.mailService = mailService;
   }
@@ -69,6 +61,7 @@ public class SuperAdminServiceFB implements SuperAdminService {
    * @throws InvalidEmailException если указанный "email" не является email
    */
   @Override
+  @Transactional
   public void create(AdminSave adminSave) throws AlreadyExistsException, InvalidEmailException {
 
     // Проверка строки email на соответствие формату email
@@ -83,9 +76,9 @@ public class SuperAdminServiceFB implements SuperAdminService {
         userRepository::existsByEmail, userEntityFromSave.getEmail());
 
     userEntityFromSave.setId(RandomValue.randomString(R.ID_LENGTH));
-    userEntityFromSave.addRole(RDB.ROLE_ADMIN);
 
     userRepository.save(userEntityFromSave);
+    userRepository.addRoleToUser(userEntityFromSave.getId(), RDB.ID_ROLE_ADMIN);
     log.info("DB. Administrator with email \"{}\" created.", adminSave.getEmail());
   }
 
@@ -95,24 +88,23 @@ public class SuperAdminServiceFB implements SuperAdminService {
    * @throws NotFoundException если администратор с указанным ID не найден
    */
   @Override
+  @Transactional
   public void delete(String idAdmin) throws NotFoundException {
 
-    // Получение пользователя по его ID, если пользователь существует
-    String prepareIdAdmin = dbIntegrityService.preparePostgreSQLString(idAdmin);
-    UserEntity userEntityFromRequest = getUserEntity(prepareIdAdmin, "Administrator");
+    // Проверка на существование пользователя или администратора по ID
+    String preparedIdAdmin = dbIntegrityService.preparePostgreSQLString(idAdmin);
+    dbIntegrityService.checkExistenceById(userRepository::existsById, preparedIdAdmin);
 
     // Проверка наличия роли "ROLE_ADMIN" у пользователя
-    checkHasRoleElseThrowNotFound(userEntityFromRequest, RDB.ROLE_ADMIN);
+    checkHasRoleElseThrowNotFound(preparedIdAdmin, RDB.ID_ROLE_ADMIN);
 
     // Если есть роль "ROLE_USER", удаление роли "ROLE_ADMIN", иначе удаление пользователя и его токена восстановления с кодом входа
-    if (userEntityFromRequest.hasRole(RDB.ROLE_USER)) {
-      userEntityFromRequest.deleteRole(RDB.ROLE_ADMIN);
-      userRepository.save(userEntityFromRequest);
+    if (userRepository.hasUserRole(preparedIdAdmin, RDB.ID_ROLE_ADMIN)) {
+      userRepository.deleteRoleFromUser(preparedIdAdmin, RDB.ID_ROLE_ADMIN);
       log.info("DB. Administrator with ID \"{}\" updated. Removed role \"{}\".", idAdmin, RDB.NAME_ROLE_ADMIN);
     } else {
-      userRepository.deleteById(prepareIdAdmin);
+      userRepository.deleteById(preparedIdAdmin);
       log.info("DB. Administrator with ID \"{}\" deleted.", idAdmin);
-      tokenCodeService.removeTokenCodeByIdUser(prepareIdAdmin);
     }
   }
 
@@ -123,20 +115,19 @@ public class SuperAdminServiceFB implements SuperAdminService {
    * @throws AlreadyExistsException если пользователь имеет роль "ROLE_ADMIN"
    */
   @Override
+  @Transactional
   public void fromUser(String idUser) throws NotFoundException, AlreadyExistsException {
 
-    // Получение пользователя по его ID, если пользователь существует
-    String prepareIdUser = dbIntegrityService.preparePostgreSQLString(idUser);
-    UserEntity userEntityFromRequest = getUserEntity(prepareIdUser, "User");
+    // Проверка существования пользователя по ID
+    String preparedIdUser = dbIntegrityService.preparePostgreSQLString(idUser);
+    dbIntegrityService.checkExistenceById(userRepository::existsById, preparedIdUser);
 
     // Проверка наличия роли "ROLE_USER" у пользователя
-    checkHasRoleElseThrowNotFound(userEntityFromRequest, RDB.ROLE_USER);
+    checkHasRoleElseThrowNotFound(preparedIdUser, RDB.ID_ROLE_USER);
     // Проверка отсутствия роли "ROLE_ADMIN" у пользователя
-    checkNotHasRoleElseThrowUserRole(userEntityFromRequest, RDB.ROLE_ADMIN);
+    checkNotHasRoleElseThrowUserRole(preparedIdUser, RDB.ID_ROLE_ADMIN);
 
-    userEntityFromRequest.addRole(RDB.ROLE_ADMIN);
-
-    userRepository.save(userEntityFromRequest);
+    userRepository.addRoleToUser(preparedIdUser, RDB.ID_ROLE_ADMIN);
     log.info("DB. User with ID \"{}\" updated. Added role \"{}\"", idUser, RDB.NAME_ROLE_ADMIN);
   }
 
@@ -147,20 +138,19 @@ public class SuperAdminServiceFB implements SuperAdminService {
    * @throws AlreadyExistsException если пользователь имеет роль "ROLE_USER"
    */
   @Override
+  @Transactional
   public void toUser(String idAdmin) throws NotFoundException, AlreadyExistsException {
 
-    // Получение пользователя по его ID, если пользователь существует
-    String prepareIdAdmin = dbIntegrityService.preparePostgreSQLString(idAdmin);
-    UserEntity userEntityFromRequest = getUserEntity(prepareIdAdmin, "Administrator");
+    // Проверка существования пользователя по ID
+    String preparedIdAdmin = dbIntegrityService.preparePostgreSQLString(idAdmin);
+    dbIntegrityService.checkExistenceById(userRepository::existsById, preparedIdAdmin);
 
     // Проверка наличия роли "ROLE_ADMIN" у пользователя
-    checkHasRoleElseThrowNotFound(userEntityFromRequest, RDB.ROLE_ADMIN);
+    checkHasRoleElseThrowNotFound(preparedIdAdmin, RDB.ID_ROLE_ADMIN);
     // Проверка отсутствия роли "ROLE_USER" у пользователя
-    checkNotHasRoleElseThrowUserRole(userEntityFromRequest, RDB.ROLE_USER);
+    checkNotHasRoleElseThrowUserRole(preparedIdAdmin, RDB.ID_ROLE_USER);
 
-    userEntityFromRequest.addRole(RDB.ROLE_USER);
-
-    userRepository.save(userEntityFromRequest);
+    userRepository.addRoleToUser(preparedIdAdmin, RDB.ID_ROLE_USER);
     log.info("DB. Administrator with ID \"{}\" updated. Added role \"{}\"", idAdmin, RDB.NAME_ROLE_USER);
   }
 
@@ -170,65 +160,49 @@ public class SuperAdminServiceFB implements SuperAdminService {
    * @throws NotFoundException если администратор с данным ID не найден
    */
   @Override
+  @Transactional
   public void toSuper(String idAdmin) throws NotFoundException {
 
-    // Получение пользователя по его ID, если пользователь существует
-    String prepareIdAdmin = dbIntegrityService.preparePostgreSQLString(idAdmin);
-    UserEntity userEntityFromRequest = getUserEntity(prepareIdAdmin, "Administrator");
+    // Проверка существования пользователя по ID
+    String preparedIdAdmin = dbIntegrityService.preparePostgreSQLString(idAdmin);
+    dbIntegrityService.checkExistenceById(userRepository::existsById, preparedIdAdmin);
 
     // Проверка наличия роли "ROLE_ADMIN" у пользователя
-    checkHasRoleElseThrowNotFound(userEntityFromRequest, RDB.ROLE_ADMIN);
+    checkHasRoleElseThrowNotFound(preparedIdAdmin, RDB.ID_ROLE_ADMIN);
 
     // Передача роли "ROLE_SUPER_ADMIN"
     UserEntity userEntitySuperAdmin = userRepository.getSuperAdmin();
-    userEntitySuperAdmin.deleteRole(RDB.ROLE_SUPER_ADMIN);
-    userEntityFromRequest.addRole(RDB.ROLE_SUPER_ADMIN);
+    userRepository.deleteRoleFromUser(userEntitySuperAdmin.getId(), RDB.ID_ROLE_SUPER_ADMIN);
+    userRepository.addRoleToUser(preparedIdAdmin, RDB.ID_ROLE_SUPER_ADMIN);
 
-    userRepository.save(userEntitySuperAdmin);
-    log.info("DB. Administrator with ID \"{}\" updated. Removed role \"{}\"", idAdmin, RDB.ROLE_SUPER_ADMIN);
-    userRepository.save(userEntityFromRequest);
     log.info("DB. Administrator with ID \"{}\" updated. Added role \"{}\"", idAdmin, RDB.ROLE_SUPER_ADMIN);
   }
 
   /**
-   * Возвращает модель пользователя по его ID
-   * @param prepareId подготовленное для бд ID пользователя
-   * @param nameObject название объекта, который удаляется: Администратор или пользователь
-   * @return модель пользователя
-   * @throws NotFoundException если пользователь не найден
-   */
-  private UserEntity getUserEntity(String prepareId, String nameObject) throws NotFoundException {
-
-    return userRepository.findById(prepareId)
-        .orElseThrow(() -> new NotFoundException(String.format(
-            "%s with ID \"%s\" not found",nameObject, prepareId)));
-  }
-
-  /**
    * Проверяет наличие у пользователя роли. В случае отсутствия, выбрасывается {@link NotFoundException}
-   * @param userEntity модель пользователя, у которого необходимо проверить наличие роли
-   * @param roleEntity модель проверяемой роли
+   * @param idUser ID проверяемого пользователя
+   * @param idRole ID проверяемой роли
    * @throws NotFoundException если пользователь с данной ролью не найден
    */
-  private void checkHasRoleElseThrowNotFound(UserEntity userEntity, RoleEntity roleEntity) throws NotFoundException {
+  private void checkHasRoleElseThrowNotFound(String idUser, String idRole) throws NotFoundException {
 
-    if (!userEntity.hasRole(roleEntity)) {
+    if (!userRepository.hasUserRole(idUser, idRole)) {
       throw new NotFoundException(String.format(
-          "User with ID \"%s\" doesn't have role with ID \"%s\"", userEntity.getId(), roleEntity.getId()));
+          "User with ID \"%s\" doesn't have role with ID \"%s\"", idUser, idRole));
     }
   }
 
   /**
    * Проверяет отсутствие у пользователя роли. В случае наличия, выбрасывается {@link AlreadyExistsException}
-   * @param userEntity модель пользователя, у которого необходимо проверить отсутствие роли
-   * @param roleEntity модель проверяемой роли
+   * @param idUser ID проверяемого пользователя
+   * @param idRole ID проверяемой роли
    * @throws AlreadyExistsException если пользователь имеет роль
    */
-  private void checkNotHasRoleElseThrowUserRole(UserEntity userEntity, RoleEntity roleEntity) throws AlreadyExistsException {
+  private void checkNotHasRoleElseThrowUserRole(String idUser, String idRole) throws AlreadyExistsException {
 
-    if (userEntity.hasRole(roleEntity)) {
+    if (userRepository.hasUserRole(idUser, idRole)) {
       throw new AlreadyExistsException(String.format(
-          "User with ID \"%s\" already has role with ID \"%s\"", userEntity.getId(), roleEntity.getId()));
+          "User with ID \"%s\" already has role with ID \"%s\"", idUser, idRole));
     }
   }
 }
