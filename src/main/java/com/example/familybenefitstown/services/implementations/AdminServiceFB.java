2713463f863fb861d025d3a9b03541de08d9c2ae
malegman <smegovic@gmail.com>
2022-03-16 13:37:01 +0500

    Изменение работы с ролями

--- src/main/java/com/example/familybenefitstown/services/implementations/AdminServiceFB.java
+++ src/main/java/com/example/familybenefitstown/services/implementations/AdminServiceFB.java
@@ -4,6 +4,7 @@ import com.example.familybenefitstown.api_models.admin.AdminInfo;
 import com.example.familybenefitstown.api_models.admin.AdminSave;
 import com.example.familybenefitstown.converters.AdminDBConverter;
 import com.example.familybenefitstown.dto.entities.UserEntity;
+import com.example.familybenefitstown.dto.repositories.RoleRepository;
 import com.example.familybenefitstown.dto.repositories.UserRepository;
 import com.example.familybenefitstown.exceptions.AlreadyExistsException;
 import com.example.familybenefitstown.exceptions.InvalidEmailException;
@@ -28,6 +29,11 @@ public class AdminServiceFB implements AdminService {
   private final UserRepository userRepository;
 
   /**
+   * Репозиторий, работающий с моделью таблицы "role"
+   */
+  private final RoleRepository roleRepository;
+
+  /**
    * Интерфейс сервиса, отвечающего за целостность базы данных
    */
   private final DBIntegrityService dbIntegrityService;
@@ -39,14 +45,17 @@ public class AdminServiceFB implements AdminService {
   /**
    * Конструктор для инициализации интерфейсов репозиториев и сервисов
    * @param userRepository репозиторий, работающий с моделью таблицы "user"
+   * @param roleRepository репозиторий, работающий с моделью таблицы "role"
    * @param dbIntegrityService интерфейс сервиса, отвечающего за целостность базы данных
    * @param mailService интерфейс сервиса для отправки сообщений на электронную почту
    */
   @Autowired
   public AdminServiceFB(UserRepository userRepository,
+                        RoleRepository roleRepository,
                         DBIntegrityService dbIntegrityService,
                         MailService mailService) {
     this.userRepository = userRepository;
+    this.roleRepository = roleRepository;
     this.dbIntegrityService = dbIntegrityService;
     this.mailService = mailService;
   }
@@ -60,14 +69,13 @@ public class AdminServiceFB implements AdminService {
   @Override
   public AdminInfo read(String idAdmin) throws NotFoundException {
 
-    String prepareIdAdmin = dbIntegrityService.preparePostgreSQLString(idAdmin);
+    String preparedIdAdmin = dbIntegrityService.preparePostgreSQLString(idAdmin);
 
-    // Получение администратора по его ID, если администратора существует
-    UserEntity userEntityFromRequest = userRepository.findById(prepareIdAdmin)
-        .orElseThrow(() -> new NotFoundException(String.format(
-            "Administrator with ID \"%s\" not found", idAdmin)));
+    // Получение администратора по его ID, если администратор существует
+    UserEntity userEntityFromRequest = userRepository.findById(preparedIdAdmin).orElseThrow(
+        () -> new NotFoundException(String.format("Administrator with ID \"%s\" not found", idAdmin)));
 
-    return AdminDBConverter.toInfo(userEntityFromRequest);
+    return AdminDBConverter.toInfo(userEntityFromRequest, roleRepository.findAllByIdUser(preparedIdAdmin));
   }
 
   /**
@@ -92,12 +100,11 @@ public class AdminServiceFB implements AdminService {
 
     // Проверка отсутствия пользователя с отличным от данного ID и данным email
     dbIntegrityService.checkAbsenceAnotherByUniqStr(
-        userRepository::existsByIdIsNotAndName, prepareIdAdmin, userEntityFromSave.getEmail());
+        userRepository::existsByIdIsNotAndEmail, prepareIdAdmin, userEntityFromSave.getEmail());
 
     // Получение администратора по его ID, если администратора существует
-    UserEntity userEntityFromDB = userRepository.findById(prepareIdAdmin)
-        .orElseThrow(() -> new NotFoundException(String.format(
-            "Administrator with ID \"%s\" not found", userEntityFromSave.getId())));
+    UserEntity userEntityFromDB = userRepository.findById(prepareIdAdmin).orElseThrow(
+        () -> new NotFoundException(String.format("Administrator with ID \"%s\" not found", userEntityFromSave.getId())));
 
     userEntityFromDB.setEmail(userEntityFromSave.getEmail());
     userEntityFromDB.setName(userEntityFromSave.getName());
