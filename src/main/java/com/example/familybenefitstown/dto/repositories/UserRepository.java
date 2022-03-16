96ec0e5d7255dfd0fd920c89840b677c1dff9443
malegman <smegovic@gmail.com>
2022-03-16 13:33:23 +0500

    Добавление методов для работы с ролями и детьми. Удаление метода получения по email

--- src/main/java/com/example/familybenefitstown/dto/repositories/UserRepository.java
+++ src/main/java/com/example/familybenefitstown/dto/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.familybenefitstown.dto.repositories;
 
 import com.example.familybenefitstown.dto.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
@@ -19,13 +20,6 @@ public interface UserRepository extends JpaRepository<UserEntity, String> {
   Optional<UserEntity> findByEmail(String email);
 
   /**
-   * Возвращает пользователя по email
-   * @param email email пользователя
-   * @return найденный пользователь по email
-   */
-  UserEntity getByEmail(String email);
-
-  /**
    * Проверяет наличие пользователя с указанным email
    * @param email email пользователя
    * @return {@code true}, если пользователь с указанным email существует
@@ -38,7 +32,7 @@ public interface UserRepository extends JpaRepository<UserEntity, String> {
    * @param email email пользователя
    * @return {@code true}, если пользователь с отличным ID и указанным email существует
    */
-  boolean existsByIdIsNotAndName(String id, String email);
+  boolean existsByIdIsNotAndEmail(String id, String email);
 
   /**
    * Возвращает пользователя с ролью "ROLE_SUPER_ADMIN"
@@ -50,5 +44,54 @@ public interface UserRepository extends JpaRepository<UserEntity, String> {
           "INNER JOIN family_benefit_town.role ON family_benefit_town.users_roles.id_role = family_benefit_town.role.id " +
           "WHERE family_benefit_town.role.name LIKE 'ROLE_SUPER_ADMIN';")
   UserEntity getSuperAdmin();
+
+  /**
+   * Создает связь между существующими ребенком и пользователем, по их ID
+   * @param idUser ID пользователя
+   * @param idChild ID ребенка
+   */
+  @Modifying
+  @Query(nativeQuery = true,
+      value = "INSERT INTO family_benefit_town.users_children (id_user, id_child) VALUES (?1, ?2);")
+  void addChildToUser(String idUser, String idChild);
+
+  /**
+   * Удаляет связь между пользователем и детьми, по его ID
+   * @param idUser ID пользователя
+   */
+  @Modifying
+  @Query(nativeQuery = true,
+      value = "DELETE FROM family_benefit_town.users_children WHERE (id_user = ?1);")
+  void deleteAllChildrenFromUser(String idUser);
+
+  /**
+   * Проверяет наличие роли у пользователя по их ID
+   * @param idUser ID пользователя
+   * @param idRole ID роли
+   * @return true, если найден пользователь с указанной ролью
+   */
+  @Query(nativeQuery = true,
+      value = "SELECT EXISTS(SELECT * FROM family_benefit_town.users_roles WHERE (id_user = ?1 AND id_role = ?2))")
+  boolean hasUserRole(String idUser, String idRole);
+
+  /**
+   * Создает связь между существующими ролью и пользователем, по их ID
+   * @param idUser ID пользователя
+   * @param idRole ID роли
+   */
+  @Modifying
+  @Query(nativeQuery = true,
+      value = "INSERT INTO family_benefit_town.users_roles (id_user, id_role) VALUES (?1, ?2);")
+  void addRoleToUser(String idUser, String idRole);
+
+  /**
+   * Удаляет связь между существующими ролью и пользователем, по их ID
+   * @param idUser ID пользователя
+   * @param idRole ID роли
+   */
+  @Modifying
+  @Query(nativeQuery = true,
+      value = "DELETE FROM family_benefit_town.users_roles WHERE (id_user = ?1 AND id_role = ?2);")
+  void deleteRoleFromUser(String idUser, String idRole);
 }
 
