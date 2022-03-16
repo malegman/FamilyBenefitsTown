14ea442c6d866a296c621e07db04b6f74f566e0e
malegman <smegovic@gmail.com>
2022-03-16 13:32:46 +0500

    Замена множества на список

--- src/main/java/com/example/familybenefitstown/dto/repositories/RoleRepository.java
+++ src/main/java/com/example/familybenefitstown/dto/repositories/RoleRepository.java
@@ -14,11 +14,12 @@ public interface RoleRepository extends JpaRepository<RoleEntity, String> {
   /**
    * Возвращает список ролей по ID пользователя
    * @param idUser ID пользователя
-   * @return множество ролей
+   * @return список ролей
    */
   @Query(nativeQuery = true,
       value = "SELECT family_benefit_town.role.id, family_benefit_town.role.name " +
-          "FROM family_benefit_town.users_roles INNER JOIN family_benefit_town.role ON family_benefit_town.users_roles.id_role = family_benefit_town.role.id " +
+          "FROM family_benefit_town.users_roles " +
+          "INNER JOIN family_benefit_town.role ON family_benefit_town.users_roles.id_role = family_benefit_town.role.id " +
           "WHERE family_benefit_town.users_roles.id_user = ?;")
   List<RoleEntity> findAllByIdUser(String idUser);
 }
