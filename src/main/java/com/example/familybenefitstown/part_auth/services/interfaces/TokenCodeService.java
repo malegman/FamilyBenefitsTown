aa515079bff20e816919c16f05d60275c6ee8fdc
malegman <smegovic@gmail.com>
2022-03-29 22:00:27 +0500

    Оптимизация методов, добавление проверки и удаления кода входа

--- src/main/java/com/example/familybenefitstown/part_auth/services/interfaces/TokenCodeService.java
+++ src/main/java/com/example/familybenefitstown/part_auth/services/interfaces/TokenCodeService.java
@@ -1,57 +1,14 @@
 package com.example.familybenefitstown.part_auth.services.interfaces;
 
-import com.example.familybenefitstown.dto.entities.RoleEntity;
 import com.example.familybenefitstown.exceptions.DateTimeException;
 import com.example.familybenefitstown.exceptions.NotFoundException;
-import com.example.familybenefitstown.part_auth.models.AuthTokens;
+import com.example.familybenefitstown.part_auth.models.AuthData;
 import com.example.familybenefitstown.part_auth.models.JwtUserData;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
-
 /**
- * Интерфейс сервиса для работы с токенами доступа (в формате jwt) и восстановления и кодом для входа
+ * Интерфейс сервиса для работы с токенами доступа (в формате jwt) и восстановления
  */
 public interface TokenCodeService {
-
-  /**
-   * Генерирует jwt для пользователя на основе его ID, ролей и IP-адреса запроса на вход систему
-   * @param id ID пользователя
-   * @param roleEntityList список ролей пользователя
-   * @return сгенерированный jwt
-   */
-  String generateJwt(String id, List<RoleEntity> roleEntityList);
-
-  /**
-   * Генерирует jwt для пользователя на основе его ID, ролей и IP-адреса запроса на вход систему
-   * @param userData данные доступа из токена доступа jwt
-   * @return сгенерированный jwt
-   */
-  String generateJwt(JwtUserData userData);
-
-  /**
-   * Извлекает данные пользователя из строки, формата токена jwt
-   * @param jwt токен пользователя, jwt
-   * @return данные авторизации
-   * @throws RuntimeException если не удалось извлечь данные пользователя из строки
-   */
-  JwtUserData dataFromJwt(String jwt) throws RuntimeException;
-
-  /**
-   * Генерирует и сохраняет токен восстановления указанной длины из символов A-Za-z0-9 для указанного пользователя
-   * @param idUser ID пользователя
-   * @return сгенерированный токен восстановления
-   */
-  String generateAndSaveRefreshToken(String idUser);
-
-  /**
-   * Возвращает ID пользователя, который владеет указанным токеном восстановления
-   * @param refreshToken токена восстановления пользователя
-   * @return {@code true}, если токен восстановления пользователя есть в бд
-   * @throws NotFoundException если токен восстановления не найден
-   * @throws DateTimeException если полученный токен восстановления истек
-   */
-  String getIdOfNotExpiredRefreshToken(String refreshToken) throws NotFoundException, DateTimeException;
 
   /**
    * Генерирует и сохраняет код для входа в систему для указанного пользователя
@@ -61,45 +18,52 @@ public interface TokenCodeService {
   int generateAndSaveLoginCode(String idUser);
 
   /**
-   * Извлекает токен доступа jwt из запроса клиента. {@code null}, если токен не найден
-   * @param request запрос клиента
-   * @return токен доступа пользователя.
-   */
-  String jwtFromRequest(HttpServletRequest request);
-
-  /**
-   * Извлекает токен восстановления из запроса клиента. {@code null}, если токен не найден
-   * @param request запрос клиента
-   * @return токен восстановления пользователя
-   */
-  String refreshTokenFromRequest(HttpServletRequest request);
-
-  /**
-   * Генерирует новые токены доступа (jwt) и восстановления по коду входа пользователя, полученного по почте
-   * @param emailLoginCode код входа пользователя
-   * @return контейнер с токенами доступа (jwt) и восстановления
-   */
-  AuthTokens generateAuthTokens(int emailLoginCode) throws NotFoundException, DateTimeException;
-
-  /**
    * Генерирует новые токены доступа (jwt) и восстановления по ID пользователя.
    * Сгенерированный токен восстановления сохраняется в бд.
+   * Существование пользователя по ID не проверяется.
    * @param idUser ID пользователя
    * @return контейнер с токенами доступа (jwt) и восстановления
-   * @throws NotFoundException если пользователь с указанным ID не найден
    */
-  AuthTokens generateAuthTokens(String idUser) throws NotFoundException;
+  AuthData generateAndSaveAuthTokens(String idUser);
+
+  /**
+   * Проверяет jwt. Если jwt корректный, возвращает данные пользователя из строки, формата токена jwt
+   * @param jwt токен пользователя, jwt
+   * @return данные пользователя, если jwt корректный
+   * @throws RuntimeException если не удалось извлечь данные пользователя из строки
+   */
+  JwtUserData checkJwt(String jwt) throws RuntimeException;
+
+  /**
+   * Проверяет токен восстановления. Если токен корректный, возвращает ID пользователя, который владеет указанным токеном восстановления
+   * @param refreshToken токен восстановления пользователя
+   * @return ID пользователя - владельца токена, если токен корректный
+   * @throws NotFoundException если токен восстановления не найден
+   * @throws DateTimeException если полученный токен восстановления истек
+   */
+  String checkRefreshToken(String refreshToken) throws NotFoundException, DateTimeException;
+
+  /**
+   * Проверяет код входа. Если код корректный, возвращает ID пользователя, который владеет указанным кодом входа
+   * @param loginCode код входа пользователя
+   * @return ID пользователя - владельца кода, если код корректный
+   * @throws NotFoundException если код входа не найден
+   * @throws DateTimeException если полученный код входа истек
+   */
+  String checkLoginCode(int loginCode) throws NotFoundException, DateTimeException;
 
   /**
    * Удаляет токен восстановления по ID пользователя
    * @param refreshToken токен восстановления пользователя
+   * @throws NotFoundException если токен восстановления не найден
    */
-  void removeRefreshToken(String refreshToken);
+  void removeRefreshToken(String refreshToken) throws NotFoundException;
 
   /**
-   * Удаляет код входа по ID пользователя
-   * @param idUser ID пользователя
+   * Удаляет код входа по его значения
+   * @param loginCode код входа пользователя
+   * @throws NotFoundException если код входа не найден
    */
-  void removeLoginCodeByIdUser(String idUser);
+  void removeLoginCode(int loginCode) throws NotFoundException;
 }
 
