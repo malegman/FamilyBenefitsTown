80464336f6ff9967a7b7946b683d7ffd43f27615
malegman <smegovic@gmail.com>
2022-03-29 21:56:14 +0500

    Добавление проверки существования по коду и удаления по коду

--- src/main/java/com/example/familybenefitstown/dto/repositories/LoginCodeRepository.java
+++ src/main/java/com/example/familybenefitstown/dto/repositories/LoginCodeRepository.java
@@ -2,6 +2,7 @@ package com.example.familybenefitstown.dto.repositories;
 
 import com.example.familybenefitstown.dto.entities.LoginCodeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.Optional;
 
@@ -11,9 +12,23 @@ import java.util.Optional;
 public interface LoginCodeRepository extends JpaRepository<LoginCodeEntity, String> {
 
   /**
+   * Проверяет наличие модели кода для входа по его значению
+   * @param loginCode значение кода для входа
+   * @return true, если модель с указанным кодом существует
+   */
+  boolean existsByCode(int loginCode);
+
+  /**
    * Возвращает модель кода для входа по значению кода пользователя
    * @param code пользовательский код для входа в систему
    * @return модель кода для входа или {@code empty}, если модель не найдена
    */
   Optional<LoginCodeEntity> findByCode(int code);
+
+  /**
+   * Удаляет код для входа по его значению
+   * @param loginCode код для входа
+   */
+  @Modifying
+  void deleteByCode(int loginCode);
 }
