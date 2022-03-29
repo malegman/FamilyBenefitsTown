4c15a21e300642dda33bc2fb3342ba1319391d4c
malegman <smegovic@gmail.com>
2022-03-29 22:01:20 +0500

    Создание поддержки для работы с заголовками http

---
+++ src/main/java/com/example/familybenefitstown/part_auth/HttpHeadersSupport.java
@@ -0,0 +1,87 @@
+package com.example.familybenefitstown.part_auth;
+
+import com.example.familybenefitstown.part_auth.models.AuthData;
+import com.example.familybenefitstown.resources.R;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Предоставляет статические методы для обработки http заголовков, работы с токенами безопасности
+ */
+public class HttpHeadersSupport {
+
+  /**
+   * Удаляет токен восстановления из Cookie refresh в http ответе
+   * @param response http ответ, из которого необходимо удалить Cookie refresh
+   * @return http ответ с удаленным токеном восстановления
+   */
+  public static HttpServletResponse removeRefreshToken(HttpServletResponse response) {
+
+    Cookie cookie = new Cookie(R.REFRESH_NAME_COOKIE, "");
+    cookie.setMaxAge(0);
+    cookie.setHttpOnly(true);
+    response.addCookie(cookie);
+
+    return response;
+  }
+
+  /**
+   * Устанавливает в http ответ токен доступа в заголовок Authorization и токен восстановления в Cookie refresh
+   * @param response http ответ, в который необходимо установить токены
+   * @param authData объект, содержащий токены
+   * @return http ответ с установленными токенами
+   */
+  public static HttpServletResponse setTokens(HttpServletResponse response, AuthData authData) {
+
+    response.addHeader(R.AUTHORIZATION_HEADER,
+                                 String.format(R.AUTHORIZATION_VALUE_PATTERN, authData.getJwtData().getTokenJwt()));
+    Cookie cookie = new Cookie(R.REFRESH_NAME_COOKIE, authData.getRefreshToken());
+    cookie.setMaxAge((int) R.REFRESH_EXPIRATION_SEC);
+    cookie.setHttpOnly(true);
+    response.addCookie(cookie);
+
+    return response;
+  }
+
+  /**
+   * Извлекает токен доступа jwt из http запроса. {@code null}, если токен не найден
+   * @param request http запрос
+   * @return токен доступа пользователя
+   */
+  public static String getJwt(HttpServletRequest request) {
+
+    if (request == null) {
+      return null;
+    }
+
+    String bearer = request.getHeader(R.AUTHORIZATION_HEADER);
+    if (StringUtils.hasText(bearer) && bearer.startsWith(R.ACCESS_TOKEN_PREFIX)) {
+      return bearer.substring(R.ACCESS_TOKEN_PREFIX.length());
+    }
+
+    return null;
+  }
+
+  /**
+   * Извлекает токен восстановления из http запроса. {@code null}, если токен не найден
+   * @param request http запрос
+   * @return токен восстановления пользователя
+   */
+  public static String getRefreshToken(HttpServletRequest request) {
+
+    if (request == null) {
+      return null;
+    }
+
+    for (Cookie cookie : request.getCookies()) {
+      if (cookie.getName().equals(R.REFRESH_NAME_COOKIE)) {
+        return cookie.getValue();
+      }
+    }
+
+    return null;
+  }
+}
